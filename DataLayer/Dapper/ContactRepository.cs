6a7da28bd01d22bca17e8f7496f0a4a4a1fb9c1f
Kyle <kcollie@hotmail.com>
2016-08-21 18:52:54 -0400

    added contacts repository

---
+++ DataLayer/Dapper/ContactRepository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataLayer.Dapper
+{
+    public class ContactRepository : IContactRepository
+    {
+        public Contact Add(Contact contact)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Contact Find(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Contact> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Contact GetFullContact(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(Contact contact)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Contact Update(Contact contact)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DataLayer/IContactRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataLayer
+{
+    public interface IContactRepository
+    {
+        Contact Find(int id);
+        List<Contact> GetAll();
+        Contact Add(Contact contact);
+        Contact Update(Contact contact);
+        void Remove(int id);
+
+        Contact GetFullContact(int id);
+        void Save(Contact contact);
+    }
+}
